675b5da3180bb88531d2426262352314e4f52edc
Kizito Akwotiga <kakwotiga@yahoo.com>
2021-07-31 17:21:00 -0400

    update game

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let scoreValue = document.querySelector(".score");
+scoreValue.textContent = 0;
+let highScoreValue = document.querySelector(".highScore");
+highScoreValue.textContent = 0;
+const secretNumber = Math.floor(Math.random()* 5)
+console.log(secretNumber);
 
 document.querySelector('.play').addEventListener('click',function reset(){
 
@@ -5,21 +11,14 @@ document.querySelector('.play').addEventListener('click',function reset(){
 
 //  then reset the score 
 
+document.location.reload()
 // reset guessing history
 console.log('play')
 })
     
 // guessing the number here function is where we hold the number 
 
-const secretNumber = Math.floor(Math.random()* 5)
-console.log(secretNumber);
 
-// counter
-let score = 0;
-let highScore = 0;
-
-document.querySelector(".score").textContent = score;
-document.querySelector(".highScore").textContent = highScore;
 
 
 // check function 
@@ -28,19 +27,19 @@ document.querySelector(".highScore").textContent = highScore;
 document.querySelector('.check').addEventListener('click',function checkNumber(){
    
  let clue = document.getElementById('clue') 
- let inputValue = document.querySelector('input').value  
+ let inputValue = Number(document.querySelector('input').value);
+ console.log(inputValue)
  
- if(secretNumber == inputValue){
-    document.querySelector(".score").textContent = score++;
-    
+ if(secretNumber === inputValue){
+   scoreValue.textContent++
     clue.innerText = "your a winner boo"
 
  } else if(secretNumber < inputValue ){
-
      clue.innerText = "your close but your cup is overflowed"
+     scoreValue.textContent--
   
  } else {
-   
+   scoreValue.textContent--
     clue.innerText ="fill her up"
  }
 
