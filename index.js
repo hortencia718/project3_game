3cc527c65e05e9c1553162b8054990be334a1532
Kizito Akwotiga <kakwotiga@yahoo.com>
2021-07-31 17:40:55 -0400

    update game

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ let scoreValue = document.querySelector(".score");
 scoreValue.textContent = 0;
 let highScoreValue = document.querySelector(".highScore");
 highScoreValue.textContent = 0;
-const secretNumber = Math.floor(Math.random()* 5)
+const secretNumber = Math.floor(Math.random() * 5)
 console.log(secretNumber);
 
-document.querySelector('.play').addEventListener('click',function reset(){
+document.querySelector('.play').addEventListener('click', function reset() {
 
-// first change the reset bg color onclick ,
+   // first change the reset bg color onclick ,
 
-//  then reset the score 
+   //  then reset the score 
 
-document.location.reload()
-// reset guessing history
-console.log('play')
+   document.location.reload()
+   // reset guessing history
+   console.log('play')
 })
-    
+
 // guessing the number here function is where we hold the number 
 
 
@@ -24,28 +24,35 @@ console.log('play')
 // check function 
 // lets start guessing func here 
 
-document.querySelector('.check').addEventListener('click',function checkNumber(){
-   
- let clue = document.getElementById('clue') 
- let inputValue = Number(document.querySelector('input').value);
- console.log(inputValue)
- 
- if(secretNumber === inputValue){
-   scoreValue.textContent++
-    clue.innerText = "your a winner boo"
+document.querySelector('.check').addEventListener('click', function checkNumber() {
 
- } else if(secretNumber < inputValue ){
-     clue.innerText = "your close but your cup is overflowed"
-     scoreValue.textContent--
-  
- } else {
-   scoreValue.textContent--
-    clue.innerText ="fill her up"
- }
+   let clue = document.getElementById('clue')
+   let inputValue = Number(document.querySelector('input').value);
+   console.log(inputValue)
 
- 
-// grab the value the person as put in and match it to the secret number .
-    
-// compare the value to the sercet number
-    // console.log(inputValue)
-    })
+   if (secretNumber === inputValue) {
+      scoreValue.textContent++
+      clue.innerText = "your a winner boo"
+
+   } else if (secretNumber < inputValue) {
+      clue.innerText = "your close but your cup is overflowed"
+      scoreValue.textContent--
+
+   } else {
+      scoreValue.textContent--
+      clue.innerText = "fill her up"
+   }
+
+
+   // grab the value the person as put in and match it to the secret number .
+
+   // compare the value to the sercet number
+   // console.log(inputValue)
+   let newTag = document.createElement("li");
+   let child = document.getElementById("history").appendChild(newTag);
+   child.innerText = inputValue;
+
+})
+
+
+   //  history
